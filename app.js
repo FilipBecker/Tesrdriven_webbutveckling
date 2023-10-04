fc06d5d11eff42543408d940bd84f2ddffd3a90f
FilipBecker <filip.becker@edu.huddinge.se>
2023-10-04 19:05:08 +0200

    Upptäckte att app.js inte var kopplad till index.js vilket skapade felen samt kopplade dem filera.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+
 var app = express();
 
 // view engine setup
@@ -16,9 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404)); 
 });
 
 // error handler
